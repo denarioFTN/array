4663b9047485fc50fa8bf73602c8707a49e4ea22
denarioFTN <denariofortunato@gmail.com>
2024-12-26 16:37:20 +0100

    criei um arquivo php

--- base/home.php
+++ base/home.php
@@ -1,3 +1,13 @@
-<php
-
-?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>GitHub</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores quod itaque aperiam non, facilis minima quia perferendis consectetur, quam, beatae voluptatibus aut a animi eum ullam nobis at culpa unde?</p>
+    
+</body>
+</html>
